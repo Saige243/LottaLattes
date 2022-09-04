34db6d7d7849ea34d34561dcc17075e29f5ef6d9
Saige Cross <saigecrossdev@gmail.com>
2022-09-04 14:34:33 -0500

    deleted unused imports

--- src/App.js
+++ src/App.js
@@ -3,11 +3,7 @@ import TextField from "@mui/material/TextField";
 import FormControl from "@mui/material/FormControl";
 import Button from "@mui/material/Button";
 import LocalDrinkIcon from "@mui/icons-material/LocalDrink";
-
 import { useState, useEffect } from "react";
-import { LocalConvenienceStoreOutlined } from "@mui/icons-material";
-import { toHaveTextContent } from "@testing-library/jest-dom/dist/matchers";
-import useEnhancedEffect from "@mui/material/utils/useEnhancedEffect";
 
 function App() {
   const [goal, setGoal] = useState("");
